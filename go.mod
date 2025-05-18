210bdb6ce499b957325ada80fdac6950b5c3f00a
dvdmarveira <ddmdoliveira@gmail.com>
2025-05-17 23:47:36 -0300

    feat: config db

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dvdmarveira/crud-go
 
 go 1.24.3
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
 
 func Read(writer http.ResponseWriter, response *http.Request) {
 
 }
 
 func Create(writer http.ResponseWriter, response *http.Request) {
+
+}
+
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("postgres", "postgres://postgres:2513@postgres/crudgo?sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to database.")
 
 }
 
